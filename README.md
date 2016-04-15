213a4da5bc2cd6dabb018dccf998836b89c0e10a
Antonio Carlos L. Ortiz <ortizantoniocarlos@gmail.com>
2016-04-15 18:18:34 +0800

    update README.md; update admin.py

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # django-channels-chat
+a replica of (https://github.com/jacobian/channels-example)[https://github.com/jacobian/channels-example].
+
+##TODO
--- chat/rooms/admin.py
+++ chat/rooms/admin.py
@@ -1,17 +1,6 @@
 from django.contrib import admin
 from .models import Room, Message
 
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ['name', 'slug']
-#     prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Room)
-
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = ['name', 'slug', 'price', 'stock',
-#                     'available', 'created', 'updated']
-#     list_filter = ['available', 'created', 'updated']
-#     list_editable = ['price', 'stock', 'available']
-#     prepopulated_fields = {'slug': ('name',)}
-
 admin.site.register(Message)
